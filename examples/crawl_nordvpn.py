8751aef80259b605ba779b9608a4ccf41df6da93
Ilya <Ilya>
2019-05-04 08:25:33 +0200

    improved path handling

--- examples/crawl_nordvpn.py
+++ examples/crawl_nordvpn.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from bs4 import BeautifulSoup
+import os
 import requests
 
 
@@ -8,7 +9,8 @@ def web_request(url, time_out=60):
     try:
         resp = requests.get(url,
                             timeout=time_out,
-                            headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'})
+                            headers={
+                                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'})
 
         if (100 <= resp.status_code < 600) and (resp.status_code != 200):
             raise ConnectionError('Error: Bad response code')
@@ -47,17 +49,19 @@ def crawl_nordvpn(raw_content):
 
 
 def write_to_file(path, f_name, items):
+    save_dir = ''.join((os.path.abspath(path), '/'))
+
     print('Writing to file:', f_name)
     with open(path + f_name, 'w') as f:
         for item in items:
             f.write(item + '\n')
-    print('DONE')
+    print('Created:', save_dir + f_name)
 
 
 if __name__ == "__main__":
     URL = "https://nordvpn.com/ovpn"
 
-    f_path = '../'
+    f_path = '.'
     f_name = 'servers.list'
 
     data = web_request(URL).content
@@ -65,4 +69,3 @@ if __name__ == "__main__":
     servers_list = crawl_nordvpn(data)
 
     write_to_file(f_path, f_name, servers_list)
-
